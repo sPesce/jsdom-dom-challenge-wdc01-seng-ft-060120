60ed9e9ecd5665bda85e4bfbd49f35fae6b812bb
steve <stevepesce879@gmail.com>
2020-07-15 14:39:27 -0400

    finished pause button functionality

--- js/challenge.js
+++ js/challenge.js
@@ -1,11 +1,16 @@
+
+let is_paused = false;
+
 document.addEventListener("DOMContentLoaded",function domLoaded()
 {
-  var is_paused = false;
   const counter = document.getElementById("counter");
   window.setInterval(incrementCounter,1000)  
 
   const minus = document.getElementById("minus");
   const plus = document.getElementById("plus");
+  const pause = document.getElementById("pause");
+  const heart = document.getElementById("heart");
+  const submit = document.getElementById("submit");
   plus.addEventListener("click",function(e)
   {
     incrementCounter();
@@ -14,22 +19,38 @@ document.addEventListener("DOMContentLoaded",function domLoaded()
   {
     decrementCounter();
   }); 
+
   /*pauseListener*/
-  //stop counter
-  //set minus, plus, heart, submit(comment) to disabled
-  //change text on pause button to resume
-  //make resume do the opposite
-  const pause = document.getElementById("pause");
   pause.addEventListener("click",function(e)
   {
-
+    const buttons = [minus,plus,heart,submit];
+    //toggle is_paused
+    is_paused = !is_paused;
+    //toggle each buttons 'disabled' property     
+    for(const html_tag of buttons)
+    {
+      html_tag.disabled = !html_tag.disabled;
+    }   
+    if (is_paused)
+    {       
+      //change text on pause button to resume
+      pause.innerText = 'resume'
+    } else
+    {    
+      //change text on pause button to resume
+      pause.innerText = 'pause'
+    }
   });
 });
-
+//increment counters value by one, unless is_paused
 function incrementCounter()
 {
-  counter.innerText = (parseInt(counter.innerText,10) + 1)
+  if (!is_paused)
+  {    
+    counter.innerText = (parseInt(counter.innerText,10) + 1)
+  }
 }
+//decrement counter's value by one
 function decrementCounter()
 {
   counter.innerText = (parseInt(counter.innerText,10) - 1)
